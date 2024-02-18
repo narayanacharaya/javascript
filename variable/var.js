3f7e66cc57d7f3149568fd978004f9e114691fc7
narayanacharaya <acharayanarayan01@gmail.com>
2024-02-18 21:21:56 +0545

    adding good let vs var example

--- variable/var.js
+++ variable/var.js
@@ -1,9 +1,21 @@
-function outerFunction() {
-    var innerMessage = "Hello from inner function";
+// function outerFunction() {
+//     var innerMessage = "Hello from inner function";
 
-    console.log("Can be accessed from within:", innerMessage);
+//     console.log("Can be accessed from within:", innerMessage);
+// }
+// console.log("Can also be accessed from outside the function:", innerMessage);
+// outerFunction();
+
+
+function example() {
+    if (true) {
+        var varVariable = "I'm a var variable";
+        let letVariable = "I'm a let variable";
+    }
+    
+    console.log("Accessing varVariable outside the block:", varVariable);
+    // console.log("Accessing letVariable outside the block:", letVariable); // This will cause an error
 }
 
-outerFunction();
-
-console.log("Can also be accessed from outside the function:", innerMessage);
+example();
+// this is rather the best example of var vs lets
