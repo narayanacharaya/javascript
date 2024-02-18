525a32347ea08565c8f0d180155ac9d804e9afdf
narayanacharaya <acharayanarayan01@gmail.com>
2024-02-18 20:35:38 +0545

    let varibale

---
+++ variable/let.js
@@ -0,0 +1,16 @@
+let a=10;
+  function first(){
+        let first=20;
+        console.log(" sum of two varibale is",a+first);
+  }
+   first();
+// this program works fine 
+
+
+let b=10;
+  function second(){
+        let second=20;
+        console.log(" sum of two varibale is",a+first);
+  }
+   console.log(b+secondv);
+// this should give an refrence error 
