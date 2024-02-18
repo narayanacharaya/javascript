a489d1b35f43200a3c76adf878e87410b5efd570
narayanacharaya <acharayanarayan01@gmail.com>
2024-02-18 20:52:43 +0545

    Introduction to var

---
+++ variable/var.js
@@ -0,0 +1,9 @@
+function outerFunction() {
+    var innerMessage = "Hello from inner function";
+
+    console.log("Can be accessed from within:", innerMessage);
+}
+
+outerFunction();
+
+console.log("Can also be accessed from outside the function:", innerMessage);
